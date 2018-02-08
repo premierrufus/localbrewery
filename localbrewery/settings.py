869df6ccb0a76b6ff328deb99e3c6b8e030c308f
premierrufus <banded.gabriel@gmail.com>
2018-02-08 17:14:46 -0500

    installed postgres, updated brewing template files

--- localbrewery/settings.py
+++ localbrewery/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'localbrewery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'localbrewery',
+        'USER': 'hb',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
